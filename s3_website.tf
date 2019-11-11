9caed091933b061a30ae208206dd988d1451dc32
Akos Keresztes <akos.keresztes.ext@evosoft.com>
2019-11-11 11:34:33 +0100

    no test2.html

--- s3_website.tf
+++ s3_website.tf
@@ -35,20 +35,3 @@ resource "aws_s3_bucket_object" "index_html" {
   }
 
 }
-
-data "template_file" "thekey_html" {
-  template = "${file("${path.module}/thekey.tpl")}"
-  vars = {
-    the_key = aws_api_gateway_api_key.s3_website_admin_api_key.value
-  }
-}
-
-resource "aws_s3_bucket_object" "thekey_html" {
-  bucket       = "${local.fqdn}"
-  key          = "test2.html"
-  content      = data.template_file.thekey_html.rendered
-  depends_on   = [aws_s3_bucket.s3_website_bucket]
-  acl          = "public-read"
-  content_type = "text/html; charset=utf-8"
-  etag         = "${md5(data.template_file.thekey_html.rendered)}"
-}
